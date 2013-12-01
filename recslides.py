efc53fed4e79156087193ac7b30934290150f834
Pablo Rodríguez <ousia@users.noreply.github.com>
2013-12-01 20:05:01 +0100

    recslides: add usage instructions

--- recslides.py
+++ recslides.py
@@ -3,20 +3,20 @@
 #
 # Copyright 2008-2013 (C) Pablo Rodríguez
 #
-#       This program is free software; you can redistribute it and/or modify
-#       it under the terms of the GNU General Public License as published by
-#       the Free Software Foundation; either version 3 of the License, or
-#       (at your option) any later version.
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 3 of the License, or
+#  (at your option) any later version.
 #
-#       This program is distributed in the hope that it will be useful,
-#       but WITHOUT ANY WARRANTY; without even the implied warranty of
-#       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#       GNU General Public License for more details.
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
 #
-#       You should have received a copy of the GNU General Public License
-#       along with this program; if not, write to the Free Software
-#       Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
-#       MA 02110-1301, USA or see <http://www.gnu.org/licenses/gpl.html>.
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA or see <http://www.gnu.org/licenses/gpl.html>.
 
 import sys, os, time
 import gnomevfs
@@ -205,6 +205,11 @@ class rec_presentation:
         cr.rectangle(0, 0, self.page_width, self.page_height)
         cr.fill()
         self.page_selector.render(cr)
+
+    def usage():
+        print "\nrecslides-" + version + ".\nWritten by Pablo Rodríguez.\n"
+        print "Usage: recslides mypresentation.pdf"
+        sys.exit(2)
 
     def gtk_main_quit(self, widget, event):
         gtk.main_quit()
